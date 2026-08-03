e54473de25ae09c65bc6a17e4312c8bcd2a93e69
giselermf <gisele@Giseles-MacBook-Air.local>
2026-08-02 23:38:40 -0300

    working ui

--- src/main/java/com/codecrafthub/controller/CourseController.java
+++ src/main/java/com/codecrafthub/controller/CourseController.java
@@ -13,6 +13,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:3000", "http://localhost:3000"}, allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class CourseController {
     
     @Autowired
